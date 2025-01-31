ab313c4148a99a63fedcfe8bc15e7baa30161315
McIfald <ihorfedoruk@gmail.com>
2025-01-31 23:46:05 +0100

    test

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
     process.exit(1);
   }
 };
